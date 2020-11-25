f980024c113ec758ec17c658f41ffadc7db6cca5
Timur <timmab@yandex.ru>
2020-11-25 20:07:33 +0300

    Added first HomeWork

---
+++ HomeWork/HomeWork_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helper;
+namespace HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeWork.PrintWelcomeMessage("3");
+
+            Console.WriteLine("Задание :\na)Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой)\nб) *Выполните предыдущее задание, оформив вычисления расстояния между точками в виде метода;\n \n -=Считаем расстояние 1.0=- \n ");
+
+            Console.WriteLine("1. Введите координаты первой точки");
+            Console.Write("x : ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y : ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("2. Введите координаты второй точки");
+            Console.Write("x : ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y : ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            
+            //a)
+            Console.WriteLine($"a)Расстояние между точками : {(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2))).ToString("F2")}");
+            //b)
+            Console.WriteLine($"b)Расстояние между точками : { Distance(x1,y1,x2,y2).ToString("F2")}");
+
+            HomeWork.Pause();
+
+            
+        }
+        public static double Distance(double x1,double y1,double x2,double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
+}
---
+++ HomeWork/HomeWork_5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helper;
+namespace HomeWork_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeWork.PrintWelcomeMessage("5");
+
+            Console.WriteLine("Задание :\nНаписать программу, которая выводит на экран ваше имя, фамилию и город проживания.\nб) Сделать задание, только вывод организуйте в центре экрана\nв) * Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y)\n \n -=Меняем местами 1.0=- \n ");
+
+            string msg = "Тимур, Абдулаев, Москва.";
+            //a)
+            Console.WriteLine($"a){msg}");
+
+            //b)
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine($"б){msg}");
+
+            //c)
+            Console.Write("Введите отступ по горизонтали : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите отступ по вертикали : ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            WriteAtPosition(msg, x, y);
+
+            HomeWork.Pause();
+        }
+
+        public static void WriteAtPosition(string ms,int x,int y)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine($"в){ms}");
+        }
+    }
+}
