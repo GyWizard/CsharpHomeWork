793d060e67e49108e5d812f70e4bed4d1bc24071
Timur <timmab@yandex.ru>
2020-12-11 17:46:54 +0300

    Added HomeWork5

---
+++ HomeWork5/Task1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helper;
+using System.Text.RegularExpressions;
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeWork.PrintWelcomeMessage("5.1");
+
+            Console.WriteLine(@" Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 
+2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+а) без использования регулярных выражений;
+б) с использованием регулярных выражений.");
+            Console.Write("Без регулярных выражений:");
+            CheckPassword(Console.ReadLine());
+            Console.Write("С помощью регулярных выражений:");
+            CheckPasswordRegex(Console.ReadLine());
+
+            HomeWork.Pause();
+        }
+
+        public static bool CheckPassword(string password)
+        {
+           
+            if (password.Length>10||password.Length<2) // Проверка на длину
+            {
+                Console.WriteLine("Пароль должен быть от 2 до 10 символов");
+                Console.Write("Повторите попытку :");
+                return CheckPassword(Console.ReadLine());
+            }
+            if(Char.IsNumber(password[0])) // Первый символ не должен быть цифрой
+            {
+                Console.WriteLine("Первый символ не должен быть цифрой");
+                Console.Write("Повторите попытку :");
+                return CheckPassword(Console.ReadLine());
+            }
+
+            int dec;
+
+            foreach (char c in password)
+            {
+                if(!Char.IsLetterOrDigit(c))
+                {
+                    Console.WriteLine("Пароль должен состоять только из цифр и букв латинского алфавита"); // Проверяем является ли символ числом или буквой
+                    return CheckPassword(Console.ReadLine());
+                }
+                if(Char.IsLetter(c)) //  Если является буквой проверяем является ли латинской
+                {
+                    dec = (int)c;
+                    if ( !( (dec>=65&&dec<=90) || (dec >= 97 && dec <= 122) ) )  // 65=A 90=Z 97=a 122=z
+                    {
+                        Console.WriteLine("Пароль должен состоять только из букв латинского алфавита");
+                        Console.Write("Повторите попытку :");
+                        return CheckPassword(Console.ReadLine());
+                    }
+                }
+            }
+            Console.WriteLine("Пароль подходит!!!");
+            return true;
+        }
+
+        public static bool CheckPasswordRegex(string password) //С помощью выражения
+        {
+            Console.Write("Повторите попытку :");
+            if (Regex.IsMatch(password, @"^([A-za-z][0-9A-Za-z]{1,9})$", RegexOptions.Multiline))
+            {
+                Console.WriteLine("Пароль подходит!!!");
+                return true;
+            }
+            return CheckPasswordRegex(Console.ReadLine());
+        }
+    }
+}
---
+++ HomeWork5/Task3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helper;
+
+
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeWork.PrintWelcomeMessage("5.3");
+
+            Console.WriteLine(@" Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой. Регистр можно не учитывать:
+а) с использованием методов C#;
+б) *разработав собственный алгоритм.");
+
+            Console.WriteLine("С помощью регулярных выражений");
+
+            string s1 = Console.ReadLine();
+            string s2= Console.ReadLine();
+
+
+            IsAnagram(s1, s2); // С помощью сортировки
+            IsAnagramMy(s1, s2); //  Собственный алгоритм
+
+            HomeWork.Pause();
+        }
+
+        public static bool IsAnagram(string str1,string str2)
+        {
+            str1=str1.ToLower();  // Переводим строки в нижний регистр
+            str2 =str2.ToLower();
+
+            char[] s1 = str1.ToArray();
+            char[] s2 = str2.ToArray();
+            Array.Sort(s1);  // Сортируем строки по порядку
+            Array.Sort(s2);
+           
+            if (s1.ToString().Equals(s2.ToString()))  //   Сравниваем отсортированые строки
+            {
+                Console.WriteLine("Строки являются анаграммами");
+                return true;
+            }
+            Console.WriteLine("Строки НЕ являются анаграммами");
+            return false;
+
+        }
+
+        public static bool IsAnagramMy(string str1, string str2)  // Сравниваем числовые суммы строк
+        {
+            str1 = str1.ToLower();
+            str2 = str2.ToLower();
+            
+            if (CountString(str1)==CountString(str2))
+            {
+                Console.WriteLine("Строки являются анаграммами");
+                return true;
+            }
+            Console.WriteLine("Строки НЕ являются анаграммами");
+            return false;
+
+        }
+
+        public static int CountString(string str) // Считаем числовую сумму символов строки
+        {
+            int count = 0;
+            foreach(char s in str)
+            {
+                count += (int)s;
+            }
+            return count;
+        }
+
+    }
+}
