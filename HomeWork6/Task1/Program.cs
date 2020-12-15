e3e0855481617b68082291d81192d378257c5543
Timur <timmab@yandex.ru>
2020-12-15 17:12:01 +0300

    Added HomeWork6

---
+++ HomeWork6/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Task1
+{
+    public delegate double Fun(double a, double x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunTask task = new RunTask();
+            task.Task();
+        }
+
+     
+
+
+
+
+    }
+}
