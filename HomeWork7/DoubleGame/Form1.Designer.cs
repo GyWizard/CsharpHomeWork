47050ec609b3f6dcb101fb553a56c62153b27663
Timur <timmab@yandex.ru>
2020-12-17 16:34:56 +0300

    Added HomeWork7

---
+++ HomeWork7/DoubleGame/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace DoubleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.outTxt = new System.Windows.Forms.Label();
+            this.generateNumber = new System.Windows.Forms.Button();
+            this.numberToReach = new System.Windows.Forms.Label();
+            this.btnRewind = new System.Windows.Forms.Button();
+            this.steps = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(116, 85);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(116, 56);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(75, 23);
+            this.btnMult.TabIndex = 1;
+            this.btnMult.Text = "*2";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(116, 27);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 23);
+            this.btnPlus.TabIndex = 2;
+            this.btnPlus.Text = "+1";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // outTxt
+            // 
+            this.outTxt.AutoSize = true;
+            this.outTxt.Location = new System.Drawing.Point(37, 66);
+            this.outTxt.Name = "outTxt";
+            this.outTxt.Size = new System.Drawing.Size(13, 13);
+            this.outTxt.TabIndex = 3;
+            this.outTxt.Text = "0";
+            // 
+            // generateNumber
+            // 
+            this.generateNumber.Location = new System.Drawing.Point(40, 186);
+            this.generateNumber.Name = "generateNumber";
+            this.generateNumber.Size = new System.Drawing.Size(131, 23);
+            this.generateNumber.TabIndex = 4;
+            this.generateNumber.Text = "Начать играть";
+            this.generateNumber.UseVisualStyleBackColor = true;
+            this.generateNumber.Click += new System.EventHandler(this.generateNumber_Click);
+            // 
+            // numberToReach
+            // 
+            this.numberToReach.AutoSize = true;
+            this.numberToReach.Location = new System.Drawing.Point(84, 170);
+            this.numberToReach.Name = "numberToReach";
+            this.numberToReach.Size = new System.Drawing.Size(45, 13);
+            this.numberToReach.TabIndex = 5;
+            this.numberToReach.Text = "Число :";
+            // 
+            // btnRewind
+            // 
+            this.btnRewind.Location = new System.Drawing.Point(116, 114);
+            this.btnRewind.Name = "btnRewind";
+            this.btnRewind.Size = new System.Drawing.Size(75, 23);
+            this.btnRewind.TabIndex = 6;
+            this.btnRewind.Text = "Rewind";
+            this.btnRewind.UseVisualStyleBackColor = true;
+            this.btnRewind.Click += new System.EventHandler(this.btnRewind_Click);
+            // 
+            // steps
+            // 
+            this.steps.AutoSize = true;
+            this.steps.Location = new System.Drawing.Point(87, 216);
+            this.steps.Name = "steps";
+            this.steps.Size = new System.Drawing.Size(45, 13);
+            this.steps.TabIndex = 7;
+            this.steps.Text = "Шагов :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 256);
+            this.Controls.Add(this.steps);
+            this.Controls.Add(this.btnRewind);
+            this.Controls.Add(this.numberToReach);
+            this.Controls.Add(this.generateNumber);
+            this.Controls.Add(this.outTxt);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Label outTxt;
+        private System.Windows.Forms.Button generateNumber;
+        private System.Windows.Forms.Label numberToReach;
+        private System.Windows.Forms.Button btnRewind;
+        private System.Windows.Forms.Label steps;
+    }
+}
+
---
+++ HomeWork7/GuessGame/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessGame
+{
+    public partial class Form1 : Form
+    {
+        public int n = 0;
+        int g = 0;
+        int s = 0;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            n = r.Next(0, 100);
+        }
+
+        private void guess_Click(object sender, EventArgs e)
+        {
+            
+            if(Int32.TryParse(inText.Text, out g))
+            {
+                s++;
+                steps.Text= $"Попыток : {s}";
+                if (g > n) { outTxt.Text = "СЛИШКОМ БОЛЬШОЕ"; }
+                if (g < n) { outTxt.Text = "СЛИШКОМ МАЛЕНЬКОЕ"; }
+                if (g == n) 
+                {
+                    MessageBox.Show($"УГАДАЛИ! ЧИСЛО = {n}! Попыток : {s}");
+                    outTxt.Text = "УГАДАЙ ЧИСЛО";
+                    n = r.Next(0, 100);
+                    g = 0;
+                    s = 0;
+                    inText.Text = "";
+                }
+
+            }
+            else
+            {
+                outTxt.Text = "НУЖНО ВВЕСТИ ЧИСЛО!";
+            }
+        }
+    }
+}
